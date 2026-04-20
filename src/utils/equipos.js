2f9be746ae8e2d31839f8e712cf6c859dbc34f0f
JorgeSczP <jorgeperezsanchez084@gmail.com>
2026-04-20 14:16:45 -0600

    Cambio de diagram basque femenil

--- src/utils/equipos.js
+++ src/utils/equipos.js
@@ -1,23 +1,3 @@
-export const basquetEquipos = [
-  { cct: "", plantel: "ss", rama: "", status: "Pendiente" }
-]
-
-export const futbolEquipos = [
-  { cct: "q", plantel: "q", rama: "a", status: "a" }
-]
-
-export const volibolEquipos = [
-  { cct: "q", plantel: "q", rama: "a", status: "a" }
-]
-
-export const resistenciaEquipos = [
-  { cct: "q", plantel: "q", rama: "a", estudiante: "", status: "a" }
-]
-
-export const velocidadEquipos = [
-  { cct: "q", plantel: "q", rama: "a", estudiante: "", status: "a" }
-]
-
 export const torneoBasquetbolV = [
   {
     fecha: "Lunes 27 de abril 2026",
@@ -127,7 +107,7 @@ export const torneoBasquetbolV = [
   }
 ];
 
-export const torneoBasquetbolF = [
+export const torneoBasquetbolF2 = [
   {
     fecha: "Lunes 27 de abril 2026",
     juegos: [
@@ -232,6 +212,115 @@ export const torneoBasquetbolF = [
         visita: "Ganador juego XI",
         hora: "10:45 - 11:30"
       }
+    ]
+  }
+];
+
+export const torneoBasquetbolF = [
+  {
+    fecha: "Lunes 27 de abril 2026",
+    juegos: [
+      {
+        nombre: "Juego I",
+        local: "",
+        rL: 0,
+        rV: 0,
+        visita: "",
+        hora: "09:00 - 09:45"
+      },
+      {
+        nombre: "Juego II",
+        local: "",
+        rL: 0,
+        rV: 0,
+        visita: "",
+        hora: "09:45 - 10:30"
+      },
+      {
+        nombre: "Juego III",
+        local: "Ganador juego 1",
+        rL: 0,
+        rV: 0,
+        visita: "",
+        hora: "10:30 - 11:15"
+      },
+      {
+        nombre: "Juego IV",
+        local: "Ganador juego 2",
+        rL: 0,
+        rV: 0,
+        visita: "",
+        hora: "11:15 - 12:00"
+      },
+      {
+        nombre: "Juego V",
+        local: "Perdedor Juego I",
+        rL: 0,
+        rV: 0,
+        visita: "Perdedor Juego II",
+        hora: "12:00 - 12:45"
+      },
+      {
+        nombre: "Juego VI",
+        local: "Perdedor Juego III",
+        rL: 0,
+        rV: 0,
+        visita: "Perdedor Juego IV",
+        hora: "12:45 - 13:30"
+      },
+      {
+        nombre: "Juego VII",
+        local: "Ganador juego III",
+        rL: 0,
+        rV: 0,
+        visita: "Ganador juego IV",
+        hora: "13:30 - 14:15"
+      },
+      {
+        nombre: "Juego VIII",
+        local: "Ganador juego V",
+        rL: 0,
+        rV: 0,
+        visita: "Ganador juego VI",
+        hora: "14:15 - 15:00"
+      }
+    ]
+  },
+  {
+    fecha: "Martes 28 de abril 2026",
+    juegos: [
+      {
+        nombre: "Juego IX",
+        local: "Ganador juego VIII",
+        rL: 0,
+        rV: 0,
+        visita: "Perdedor Juego IX",
+        hora: "08:00 - 08:45"
+      },
+      {
+        nombre: "Juego X Final *",
+        local: "Ganador juego",
+        rL: 0,
+        rV: 0,
+        visita: "Ganador juego",
+        hora: "04:45 - 09:30"
+      },
+      // {
+      //   nombre: "Juego XI",
+      //   local: "",
+      //   rL: 0,
+      //   rV: 0,
+      //   visita: "",
+      //   hora: "10:00 - 10:45"
+      // },
+      // {
+      //   nombre: "Juego XII Final *",
+      //   local: "",
+      //   rL: 0,
+      //   rV: 0,
+      //   visita: "",
+      //   hora: "10:45 - 11:30"
+      // }
     ]
   }
 ];
