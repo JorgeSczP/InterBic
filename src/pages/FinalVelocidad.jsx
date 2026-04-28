606485b0c2908a6658ff82d8194beaa73be8a7e2
itzel376 <floresareli490@gmail.com>
2026-04-28 08:55:47 -0600

    Volibasquet4

--- src/pages/FinalVelocidad.jsx
+++ src/pages/FinalVelocidad.jsx
@@ -9,7 +9,7 @@ export default function FinalVelocidad() {
     <section className="py-24 min-h-screen overflow-hidden" id="disciplinas">
       <div className="mb-4 max-w-7xl mx-auto px-6">
         <div className="flex w-full justify-center items-center py-4">
-          <h1 className="text-3xl font-bold text-slate-900 mb-2">Basquetbol</h1>
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Velocidad</h1>
         </div>
       </div>
 
