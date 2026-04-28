7138066b3e8ce7b88e28fa9bcb47668698e40468
itzel376 <floresareli490@gmail.com>
2026-04-28 08:53:46 -0600

    Volibasquet3

--- src/pages/FinalVoleibol.jsx
+++ src/pages/FinalVoleibol.jsx
@@ -50,7 +50,7 @@ export default function FinalVoleibol() {
           <div className="space-y-4 ">
             <CardFinal lugar={1} equipo="" />
             <CardFinal lugar={2} equipo="" />
-            <CardFinal lugar={3} equipo="" />
+            <CardFinal lugar={3} equipo="SANTA MARÍA NUTÍO" />
           </div>
         </section>
       )}
